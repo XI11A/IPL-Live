6f9cd36f56a5021be02250e2c5a82962ca44c4f1
Nivash <65842454+XI11A@users.noreply.github.com>
2022-03-26 01:13:56 +0530

    Updated Stream URL

--- starsports1hd.js
+++ starsports1hd.js
@@ -3,7 +3,7 @@
   
     if (Hls.isSupported()) {
       var hls = new Hls();
-      hls.loadSource('http://s1.xlocalhost.tk/hls/ten.m3u8');
+      hls.loadSource('https://vcdn.xlocalhost.workers.dev/stersports1hd.m3u8');
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
