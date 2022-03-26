ab6211e2519b0cfacddfca05f58ba938faafc515
Nivash <65842454+XI11A@users.noreply.github.com>
2022-03-26 21:42:17 +0530

    Update starsports1hd.js

--- starsports1hd.js
+++ starsports1hd.js
@@ -3,7 +3,7 @@
   
     if (Hls.isSupported()) {
       var hls = new Hls();
-      hls.loadSource('https://vcdn.xlocalhost.tk/hls/stersports1hd.m3u8');
+      hls.loadSource('/hls/stersports1hd.m3u8');
       hls.attachMedia(video);
       hls.on(Hls.Events.MANIFEST_PARSED,function() {
         video.play();
